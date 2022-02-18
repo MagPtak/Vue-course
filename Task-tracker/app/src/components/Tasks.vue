985f032f2584a2c7d05456b84fa3a06361437d84
Magdalena Ptak <magdalena.ptak1990@gmail.com>
2022-02-18 16:59:45 +0100

    Add task component

--- Task-tracker/app/src/components/Tasks.vue
+++ Task-tracker/app/src/components/Tasks.vue
@@ -1,14 +1,18 @@
 <template>
   <!-- Wykonanie pętlo for na tablicy tasks, utworzenie dla kadego task elementu h3 i wyswietlenie zawartości text -->
   <div :key="task.id" v-for="task in tasks">
-    <h3>{{ task.text }}</h3>
+    <Task :task="task" />
   </div>
 </template>
 <script>
+import Task from "./Task";
 export default {
   name: "Tasks",
   props: {
     tasks: Array,
+  },
+  components: {
+    Task,
   },
 };
 </script>
