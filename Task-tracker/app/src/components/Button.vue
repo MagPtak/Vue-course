7f792583bc48c5d1c10186743160924eab7d13ba
Magdalena Ptak <magdalena.ptak1990@gmail.com>
2022-02-18 16:36:21 +0100

    Add Button component

---
+++ Task-tracker/app/src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- Skrócony zapis: zamiast v-on:click -> @:click -->
+  <button @click="onClick()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onclick() {
+      console.log("click");
+    },
+  },
+};
+</script>
+
