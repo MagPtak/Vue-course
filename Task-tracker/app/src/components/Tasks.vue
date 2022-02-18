849e18cc36c93b11235034ae83c67251775224b0
Magdalena Ptak <magdalena.ptak1990@gmail.com>
2022-02-18 16:44:36 +0100

    Create Tasks component with tasks list

---
+++ Task-tracker/app/src/components/Tasks.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- Wykonanie pętlo for na tablicy tasks, utworzenie dla kadego task elementu h3 i wyswietlenie zawartości text -->
+  <div :key="task.id" v-for="task in tasks">
+    <h3>{{ task.text }}</h3>
+  </div>
+</template>
+<script>
+export default {
+  name: "Tasks",
+  props: {
+    tasks: Array,
+  },
+};
+</script>
