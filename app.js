f7e4e16b8f7e6c43600c8b9ce454a14fe2b32bb3
Magdalena Ptak <magdalena.ptak1990@gmail.com>
2022-02-18 12:31:49 +0100

    Add getUser function to fetch user's data

--- app.js
+++ app.js
@@ -5,8 +5,21 @@ const app = Vue.createApp({
       lastName: "Doe",
       email: "john@buziaczek.pl",
       gender: "male",
-      picture: "https://picsum.photos/150/225?random=1",
+      picture: "https://randomuser.me/api/portraits/men/3.jpg",
     };
+  },
+  methods: {
+    async getUser() {
+      const res = await fetch("https://randomuser.me/api");
+      const { results } = await res.json();
+      console.log(results);
+
+      this.firstName = results[0].name.first;
+      this.lastName = results[0].name.last;
+      this.email = results[0].email;
+      this.gender = results[0].gender;
+      this.picture = results[0].picture.large;
+    },
   },
 });
 
